da7a4b52bccd966fa7b26dab9d048455b6d18530
Mauri Vazquez <mauriv_32@hotmail.com>
2022-06-29 09:53:03 -0700

    The Dicee Game Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Creating random numbers and changing images src
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").src = "images/dice" + randomNumber1 + ".png";
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").src = "images/dice" + randomNumber2 + ".png";
+
+//Changing h1 to announce which player won
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "✨ Player 1 Wins ✨"
+}else if (randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "✨ Player 2 Wins ✨"
+}else {
+  document.querySelector("h1").innerHTML = "✨ Draw! ✨"
+}
